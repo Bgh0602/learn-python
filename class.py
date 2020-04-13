cecffb66e3793a0fc72600daba8dd85e7ff26e8a
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-13 18:21:55 +0900

    Update class.py
    plus. choosing dan in control

--- class.py
+++ class.py
@@ -1,6 +1,14 @@
-print('enter dan for gugudan(2~9)')
-dan = int(input('dan:'))
+while True:
+    print('choose dan')
+    dan = int(input('dan:'))
+    if dan < 1 or dan > 9:
+        print('again')
+    if 0 < dan and dan < 10:
+        break
+print(dan, 'dan')
 count = 1
-while count < 10:
+while True:
     print(dan, '*', count, '=', dan*count)
     count += 1
+    if count > 9:
+        break
