23bd3beda715ac82cd0cf42150e88c3bc2f01c18
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-13 17:49:19 +0900

    Update class.py
    break문 사용

--- class.py
+++ class.py
@@ -1,16 +1,6 @@
-print('My name is Alice. your name?')
-myName = input()
-realDay = 10
-print(myName, '', 'guess my birthday(1~30)')
-trial = 0
-while trial <= 5:
-    trial += 1
-    guessDay = int(input('guess:'))
-    if guessDay < realDay:
-        print('guess a later day')
-    if guessDay > realDay:
-        print('guess a early day')
-    if guessDay == realDay:
-        print('correct')
+treehit = 1
+while True:
+    print('chop the tree'+str(treehit)+'time')
+    treehit += 1
+    if treehit == 3:
         break
-print('my birth day is ', realDay)
