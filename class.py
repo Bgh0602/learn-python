57cc5b426e3aa30984108dda4d106f1ee6ed959b
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-13 17:39:50 +0900

    Update class.py
    생일 맞추기

--- class.py
+++ class.py
@@ -1,11 +1,16 @@
-print('I am thinking of a number of from 10 to 50, guess it.')
-start = 10
-end = 50
-answer = 37
-guess = int(input())
-if answer > guess:
-    print('smaller than the answer')
-if answer < guess:
-    print('bigger than the answer')
-if answer == guess:
-    print('correct')
+import random
+print('My name is Alice. your name?')
+myName = input()
+realDay = random.randint(1, 30)
+print(myName, '', 'guess my birthday(1~30)')
+trial = 0
+while trial <= 5:
+    trial += 1
+    guessDay = int(input('guess:'))
+    if guessDay < realDay:
+        print('guess a later day')
+    if guessDay > realDay:
+        print('guess a early day')
+    if guessDay == realDay:
+        print('correct')
+print('my birth day is ', realDay)
