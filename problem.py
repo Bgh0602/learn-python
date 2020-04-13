5d401cd276702b608206f3d75191aaa4f777f6d8
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-13 16:58:31 +0900

    Update problem.py
    #2  문제

--- problem.py
+++ problem.py
@@ -1,9 +1,10 @@
-# 1 1~n까지의 2의 배수의 합
-sum = 0
-n = int(input('n:'))
-i = 0
-while i <= n:
-    if i % 2 == 0:
-        sum = sum+i
-    i += 1
-print('0~', n, '까지의 2의 배수의 합:', sum)
+# 2 암호 맞추기-1
+realPw = '12'
+print('enter pw(2 disit)')
+pw = 0
+while pw != realPw:
+    pw = input('pw:')
+    if pw == realPw:
+        print('Correct')
+    else:
+        print('fail, again')
