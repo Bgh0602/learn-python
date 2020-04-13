9c889ae655b188c2475acdf725d2b518619b875e
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-13 17:01:09 +0900

    Update problem.py
    #2 문제-2(using random.randint)

--- problem.py
+++ problem.py
@@ -1,9 +1,10 @@
-# 2 암호 맞추기-1
-realPw = '12'
-print('enter pw(2 disit)')
+# 2 암호 맞추기-2
+import random
+realPw = random.randint(11, 20)
+print('enter pw(11~20)')
 pw = 0
 while pw != realPw:
-    pw = input('pw:')
+    pw = int(input('pw:'))
     if pw == realPw:
         print('Correct')
     else:
